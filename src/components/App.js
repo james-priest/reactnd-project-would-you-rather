0543fe65b7ae7b9793835c86172f54582703eebe
James Priest <james-priest@outlook.com>
2019-03-31 00:38:23 -0700

    feat: Start with clean App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Learn React</header>
+      </div>
+    );
+  }
+}
+
+export default App;
