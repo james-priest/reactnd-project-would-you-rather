804827b1dbaed94bd3c5aacebcb08328268dbc33
James Priest <james-priest@outlook.com>
2019-04-01 02:58:44 -0700

    feat: Add UITest1 Header, Message Accordion, Ribbons

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import UITest from './mocks/UITest1';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">Learn React</header>
+        <UITest />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
