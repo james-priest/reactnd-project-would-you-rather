3ddad4de4421f7fb719ff57cf251448f89d65116
James Priest <james-priest@outlook.com>
2019-04-04 04:05:47 -0700

    fix: Remove unused ref

--- src/components/mocks/Home.js
+++ src/components/mocks/Home.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
-import TabControl from './TabControl';
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <Nav />
-        {/* <TabControl /> */}
       </div>
     );
   }
