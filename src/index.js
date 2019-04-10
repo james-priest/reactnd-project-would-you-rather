ecf7020423e6be877df9fbf4048136c277c95517
James Priest <james-priest@outlook.com>
2019-04-10 07:20:19 -0700

    fix: Move sui stylesheet to index to solve  missing icons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 // import 'fomantic-ui-css/semantic.min.css';
 import './index.css';
 
