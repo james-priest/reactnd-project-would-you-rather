7d2f96fee3237950dd0d89673deb98bb4c8fede2
James Priest <james-priest@outlook.com>
2019-04-15 10:58:02 -0700

    feat: Add api functions

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import {
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer
+} from './_DATA';
+
+export function getInitialData() {
+  return Promise.all([_getUsers(), _getQuestions()]).then(
+    ([users, questions]) => ({
+      users,
+      questions
+    })
+  );
+}
+
+export function saveQuestion(info) {
+  return _saveQuestion(info);
+}
+
+export function saveQuestionAnswer(info) {
+  return _saveQuestionAnswer(info);
+}
