919cb77faf8fcc70d4b06727694dbfdd3c251705
James Priest <james-priest@outlook.com>
2019-04-21 12:07:08 -0700

    fix: Add missing helpers module

---
+++ src/utils/helpers.js
@@ -0,0 +1,14 @@
+export const colors = {
+  green: {
+    name: 'green',
+    hex: '#21ba45'
+  },
+  blue: {
+    name: 'blue',
+    hex: '#2185d0'
+  },
+  grey: {
+    name: null,
+    hex: '#d4d4d5'
+  }
+};
