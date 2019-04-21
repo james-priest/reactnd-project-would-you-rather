dd8f62e37514dd4751da245ce3091d0a05f5f57c
James Priest <james-priest@outlook.com>
2019-04-21 12:13:39 -0700

    fix: Add placeholder components

---
+++ src/components/PollQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class PollQuestion extends Component {
+  render() {
+    return (
+      <div>
+        <h3>PollQuestion</h3>
+      </div>
+    );
+  }
+}
+
+export default PollQuestion;
