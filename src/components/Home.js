3056f667c7bc35f9824050a93fd96d313da91532
James Priest <james-priest@outlook.com>
2019-04-21 12:02:41 -0700

    refactor: Home, UserCard, PollTeaser components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,40 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Tab } from 'semantic-ui-react';
 import UserCard from './UserCard';
-import PollTeaser from './PollTeaser';
-
-const color = {
-  green: {
-    name: 'green',
-    hex: '#21ba45'
-  },
-  blue: {
-    name: 'blue',
-    hex: '#2185d0'
-  },
-  teal: {
-    name: 'teal',
-    hex: '#009c95'
-  },
-  grey: {
-    name: null,
-    hex: '#767676'
-  }
-};
 
 export class Home extends Component {
   static propTypes = {
-    // unansweredQuestions: PropTypes.array.isRequired,
-    // answeredQuestions: PropTypes.array.isRequired
     userQuestionData: PropTypes.object.isRequired
   };
   render() {
     const { userQuestionData } = this.props;
-    // console.log(
-    //   'this.props.unansweredQuestions',
-    //   this.props.unansweredQuestions
-    // );
-    // console.log('this.props.answeredQuestions', this.props.answeredQuestions);
+
     return <Tab panes={panes({ userQuestionData })} className="tab" />;
   }
 }
@@ -51,15 +25,9 @@ const panes = props => {
           {userQuestionData.answered.map(question => (
             <UserCard
               key={question.id}
-              userId={question.author}
-              color={color.green.hex}
-            >
-              <PollTeaser
-                question={question}
-                unanswered={true}
-                color={color.green.name}
-              />
-            </UserCard>
+              question_id={question.id}
+              unanswered={true}
+            />
           ))}
         </Tab.Pane>
       )
@@ -71,15 +39,9 @@ const panes = props => {
           {userQuestionData.unanswered.map(question => (
             <UserCard
               key={question.id}
-              userId={question.author}
-              color={color.blue.hex}
-            >
-              <PollTeaser
-                question={question}
-                unanswered={false}
-                color={color.blue.name}
-              />
-            </UserCard>
+              question_id={question.id}
+              unanswered={false}
+            />
           ))}
         </Tab.Pane>
       )
@@ -90,10 +52,10 @@ const panes = props => {
 function mapStateToProps({ authUser, users, questions }) {
   const answeredIds = Object.keys(users[authUser].answers);
   const answered = Object.values(questions)
-    .filter(question => answeredIds.includes(question.id))
+    .filter(question => !answeredIds.includes(question.id))
     .sort((a, b) => b.timestamp - a.timestamp);
   const unanswered = Object.values(questions)
-    .filter(question => !answeredIds.includes(question.id))
+    .filter(question => answeredIds.includes(question.id))
     .sort((a, b) => b.timestamp - a.timestamp);
   // console.log('answeredIds', answeredIds);
   // console.log('answeredQuestions', answeredQuestions);
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,15 +2,49 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Segment, Header, Grid, Image } from 'semantic-ui-react';
+import PollQuestion from './PollQuestion';
+import PollResult from './PollResult';
+import PollTeaser from './PollTeaser';
+import { colors } from '../utils/helpers';
+
+const pollTypes = {
+  POLL_TEASER: 'POLL_TEASER',
+  POLL_QUESTION: 'POLL_QUESTION',
+  POLL_RESULT: 'POLL_RESULT'
+};
+
+const PollContent = props => {
+  const { pollType, question, unanswered } = props;
+
+  switch (pollType) {
+    case pollTypes.POLL_TEASER:
+      return <PollTeaser question={question} unanswered={unanswered} />;
+    case pollTypes.POLL_QUESTION:
+      return <PollQuestion question={question} />;
+    case pollTypes.POLL_RESULT:
+      return <PollResult question={question} />;
+    default:
+      return;
+  }
+};
 
 export class UserCard extends Component {
   static propTypes = {
-    userId: PropTypes.string.isRequired,
-    color: PropTypes.string
+    question: PropTypes.object.isRequired,
+    author: PropTypes.object.isRequired,
+    pollType: PropTypes.string.isRequired,
+    unanswered: PropTypes.bool,
+    question_id: PropTypes.string
   };
   render() {
-    const { user, children, color } = this.props;
+    const { author, question, pollType, unanswered = null } = this.props;
     // console.log('this.props', this.props);
+
+    const tabColor = unanswered === true ? colors.green : colors.blue;
+    const borderTop =
+      unanswered === null
+        ? `1px solid ${colors.grey}`
+        : `2px solid ${tabColor.hex}`;
 
     return (
       <Segment.Group>
@@ -19,18 +53,23 @@ export class UserCard extends Component {
           textAlign="left"
           block
           attached="top"
-          style={{
-            borderTop: `2px solid ${color}`
-          }}
-          content={`${user.name} asks:`}
-        />
+          style={{ borderTop: borderTop }}
+        >
+          {author.name} asks:
+        </Header>
 
         <Grid divided padded>
           <Grid.Row>
             <Grid.Column width={5}>
-              <Image src={user.avatarURL} />
+              <Image src={author.avatarURL} />
             </Grid.Column>
-            <Grid.Column width={11}>{children}</Grid.Column>
+            <Grid.Column width={11}>
+              <PollContent
+                pollType={pollType}
+                question={question}
+                unanswered={unanswered}
+              />
+            </Grid.Column>
           </Grid.Row>
         </Grid>
       </Segment.Group>
@@ -38,11 +77,30 @@ export class UserCard extends Component {
   }
 }
 
-function mapStateToProps({ users }, props) {
-  const user = users[props.userId];
+function mapStateToProps(
+  { users, questions, authUser },
+  { match, question_id }
+) {
+  let question, pollType;
+  if (question_id !== undefined) {
+    question = questions[question_id];
+    pollType = pollTypes.POLL_TEASER;
+  } else {
+    const { question_id } = match.params;
+    question = questions[question_id];
+    const user = users[authUser];
+
+    pollType = pollTypes.POLL_QUESTION;
+    if (Object.keys(user.answers).includes(question.id)) {
+      pollType = pollTypes.POLL_RESULT;
+    }
+  }
+  const author = users[question.author];
 
   return {
-    user
+    question,
+    author,
+    pollType
   };
 }
 
