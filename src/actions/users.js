db1579f9e90e60939c97a76c013d8a0d290c4758
James Priest <james-priest@outlook.com>
2019-04-23 19:06:36 -0700

    feat: Save new poll data to store

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,6 +3,7 @@ import { addAnswerToQuestion } from '../actions/questions';
 
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
+export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER';
 
 export function receiveUsers(users) {
   return {
@@ -28,5 +29,13 @@ export function handleSaveQuestionAnswer(authUser, qid, answer) {
     return saveQuestionAnswer(authUser, qid, answer).catch(e => {
       console.warn('Error in handleSaveQuestionAnswer:', e);
     });
+  };
+}
+
+export function addQuestionToUser({ id, author }) {
+  return {
+    type: ADD_QUESTION_TO_USER,
+    id,
+    author
   };
 }
