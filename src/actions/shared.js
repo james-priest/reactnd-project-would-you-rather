ab44c365cf69dbe205c956631825c2f366248f41
James Priest <james-priest@outlook.com>
2019-04-24 22:22:31 -0700

    fix: Remove autologin

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,16 +1,16 @@
 import { getInitialData } from '../utils/api';
 import { receiveQuestions } from '../actions/questions';
 import { receiveUsers } from '../actions/users';
-import { setAuthUser } from '../actions/authUser';
+// import { setAuthUser } from '../actions/authUser';
 
-const AUTH_ID = 'joeylene';
+// const AUTH_ID = 'joeylene';
 
 export function handleInitialData() {
   return dispatch => {
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveQuestions(questions));
       dispatch(receiveUsers(users));
-      dispatch(setAuthUser(AUTH_ID));
+      // dispatch(setAuthUser(AUTH_ID));
     });
   };
 }
